739457ad2353b518a43d915aa73112162403f235
Rebecca Labitt <49698350+rebeccalabitt@users.noreply.github.com>
2020-10-16 21:25:23 -0400

    Add files via upload

---
+++ PLP3.py
@@ -0,0 +1,24 @@
+def int_variable():
+    x = 100
+    print(x)
+
+def string_variable():
+    y = "frescaisthebest"
+    print(y)
+
+def floating_point_int():
+    i = 1.5
+    print(i)
+
+def boolean_variable():
+    A = "true"
+    B = "false"
+    print(A)
+
+def len():
+    dog1 = "lab"
+    dog2 = "bulldog"
+    dog3 = "greyhound"
+    print(len())
+
+
